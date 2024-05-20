e3073d7d2d3321a60814e92a4c165f8762d22610
kimia <kimiaaa2002mahdaviii@gmail.com>
2024-05-20 14:35:28 +0330

    added function handle_boss

--- store.py
+++ store.py
@@ -51,3 +51,39 @@ def handle_customer(self, customer_name):
     product_info["inventory"] -= quantity
     self.customers.append((customer_name, chosen_product, quantity))
     print(f"Thanks for your purchase{customer_name} Your payment amount{price} ")
+
+    def handle_boss(self):
+        print("Hello, dear manager, please select the type of information you want:")
+        print("1-List of customer names")
+        print("2- number of customers")
+        print("3- Total revenue")
+        print("4- List of available products")
+        print("5- List of finished products")
+        print("6- Sara  shopping review")
+        print("7- Add new product")
+
+        choice = input("your choice: ")
+
+        if choice == "1":
+            print("List of customer names:")
+            for customer_name in self.get_customer_names():
+                print(customer_name)
+        elif choice == "2":
+            print(f"Number of customers: {len(self.customers)}")
+        elif choice == "3":
+            print(f"Total revenue: {self.income} ")
+        elif choice == "4":
+            print("List of available products:")
+            for product_name, product_info in self.inventory.items():
+                print(f"- {product_name}: {product_info['inventory']} number")
+        elif choice == "5":
+            print("List of finished products:")
+            for product_name in self.get_out_of_stock_products():
+                print(product_name)
+        elif choice == "6":
+            self.handle_boss_question_about_sara()
+        elif choice == "7":
+            self.add_new_product()
+        else:
+            print("Invalid selection.")
+        
